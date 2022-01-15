9fc2d852f542f377256b9797bf77088859b55882
Aditya Ranjan <ranjanaditya263@gmail.com>
2022-01-15 15:13:12 +0530

    Main screen and button design completed

---
+++ lib/batman_button.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+const _imageColor = Color(0xFFC88853);
+
+class BatmanButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String text;
+  final bool left;
+
+  const BatmanButton(
+      {Key? key, required this.onTap, required this.text, this.left = true})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: const Color(0xFFFDE86A),
+      child: InkWell(
+        onTap: onTap,
+        child: ClipRect(
+          child: SizedBox(
+            height: 55,
+            color: const Color(0xFFFDE86A),
+            child: Stack(
+              children: [
+                if (left)
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..translate(-10.0)
+                        ..rotateZ(vector.radians(35)),
+                      child: Image.asset(
+                        'assets/batman_logo.png',
+                        height: 40,
+                        color: _imageColor,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                Center(
+                  child: Text(
+                    text,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                if (!left)
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..translate(10.0)
+                        ..rotateZ(vector.radians(-35)),
+                      child: Image.asset(
+                        'assets/batman_logo.png',
+                        height: 40,
+                        color: _imageColor,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,10 +1,88 @@
+import 'package:batman_signup/batman_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class MainBatmanSignupApp extends StatelessWidget{
+class MainBatmanSignupApp extends StatelessWidget {
+  const MainBatmanSignupApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Theme(
+      data: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.vidalokaTextTheme(),
+      ),
+      child: const BatmanSignup(),
+    );
   }
+}
 
+class BatmanSignup extends StatelessWidget {
+  const BatmanSignup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF100F0B),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/batman_background.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/batman_alone.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2.5,
+            right: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/batman_logo.png',
+              height: 50,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                const Text(
+                  'WELCOME TO',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+                const Text(
+                  'GOTHAM CITY',
+                  style: TextStyle(color: Colors.white, fontSize: 35),
+                ),
+                const SizedBox(height: 35,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    children: [
+                      BatmanButton(onTap: (){}, text: 'LOGIN'),
+                      const SizedBox(height: 20,),
+                      BatmanButton(onTap: (){}, text: 'SIGNUP',left: false,),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
